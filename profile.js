1761726f51aefcc01bb254c225407b7840b545df
Teeraphong Vechatrakoolphaisan <101695122+Ithzz-Newton@users.noreply.github.com>
2024-01-23 17:00:23 +0700

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,48 +1,48 @@
 function displayUserData(userData) {
+    console.log(userData);
     // Update Student Information
-    document.getElementById("en_name").textContent = "Name (EN): " + userData['English Full Name / ชื่อ-นามสกุลภาษาอังกฤษ'];
-    document.getElementById("th_name").textContent = "Name (TH): " + userData['Thai Full Name / ชื่อ-นามสกุลภาษาไทย'];
-    document.getElementById("stu_id").textContent = "Student ID: " + userData['Student ID / รหัสนักศึกษา'];
-    document.getElementById("stu_email").textContent = "Email: " + userData['ที่อยู่อีเมล'];
-  
+    document.getElementById("en_name").textContent = "Name (EN): " + userData['en_name'];
+    document.getElementById("th_name").textContent = "Name (TH): " + userData['th_name'];
+    document.getElementById("stu_id").textContent = "Student ID: " + userData['stu_id'];
+    document.getElementById("stu_email").textContent = "Email: " + userData['stu_email'];
+
     // Update Simulation Test
-    document.getElementById("pre_listening").textContent = "Listening: " + userData['PRE_LISTENING'];
-    document.getElementById("pre_reading").textContent = "Reading: " + userData['PRE_READING'];
-    var preTotal = parseInt(userData['PRE_LISTENING']) + parseInt(userData['PRE_READING']);
+    document.getElementById("pre_listening").textContent = "Listening: " + userData['pre_listening'];
+    document.getElementById("pre_reading").textContent = "Reading: " + userData['pre_reading'];
+    var preTotal = parseInt(userData['pre_listening']) + parseInt(userData['pre_reading']);
     document.getElementById("pre_total").textContent = "Total: " + preTotal;
-  
+
     // Update Post-Test
-    document.getElementById("post_listening").textContent = "Listening: " + userData['POST_LISTENING'];
-    document.getElementById("post_reading").textContent = "Reading: " + userData['POST_READING'];
-    var postTotal = parseInt(userData['POST_LISTENING']) + parseInt(userData['POST_READING']);
+    document.getElementById("post_listening").textContent = "Listening: " + userData['post_listening'];
+    document.getElementById("post_reading").textContent = "Reading: " + userData['post_reading'];
+    var postTotal = parseInt(userData['post_listening']) + parseInt(userData['post_reading']);
     document.getElementById("post_total").textContent = "Total: " + postTotal;
-  }
-  
-  async function runApp() {
+}
+
+async function runApp() {
     try {
-      const profile = await liff.getProfile();
-      const line_userId = profile.userId;
-  
-      var apiUrl = 'https://script.google.com/macros/s/AKfycbyORnSxMGZf3o90on0M0xHjRCT_pfDvNKuvvbmPOzs19Blx9t-Z_0SKhXUcP7w80c3g3Q/exec'; // Replace with the actual URL of your deployed web app
-  
-      fetch(apiUrl + '?userlineid=' + line_userId)
-        .then(response => response.json())
-        .then(data => displayUserData(data))
-        .catch(error => console.error('Error fetching user data:', error));
+        const profile = await liff.getProfile();
+        const line_userId = profile.userId;
+
+        var apiUrl = 'https://script.google.com/macros/s/AKfycbyORnSxMGZf3o90on0M0xHjRCT_pfDvNKuvvbmPOzs19Blx9t-Z_0SKhXUcP7w80c3g3Q/exec'; // Replace with the actual URL of your deployed web app
+
+        fetch(apiUrl + '?userlineid=' + line_userId)
+            .then(response => response.json())
+            .then(data => displayUserData(data))
+            .catch(error => console.error('Error fetching user data:', error));
     } catch (err) {
-      console.error(err);
+        console.error(err);
     }
-  }
-  
-  liff.init(
+}
+
+liff.init(
     { liffId: "2002580879-qJ4k4gVe" },
     async () => {
-      if (liff.isLoggedIn()) {
-        await runApp();
-      } else {
-        liff.login();
-      }
+        if (liff.isLoggedIn()) {
+            await runApp();
+        } else {
+            liff.login();
+        }
     },
     (err) => console.error(err.code, err.message)
-  );
-  
+);
